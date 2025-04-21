60736e938943fec4ccd8b97357ad55f024d84403
5kdn <sakuden542+github@gmail.com>
2025-04-21 23:28:08 +0900

    - https://uu.getuploader.com/DCSJP1/download/126

---
+++ DCSWorld/Mods/aircraft/A-10C/Missions/Training/lessons.lua
@@ -0,0 +1,73 @@
+lessons =
+{
+	{
+        ["file"] = "A-10C-Training-Introduction.miz",
+        ["name"] = _(" A-10C　イントロダクション"),
+        ["description"] = _("A-10Cという機体について解説します"),
+    },
+	{
+        ["file"] = "A-10C-Training-HOTAS-Fundamentals.miz",
+        ["name"] = _("HOTAS の基礎"),
+        ["description"] = _("コクピットのコントロールコンセプトについて解説していきます"),
+    },
+	{
+        ["file"] = "A-10C-Training-Start-Up.miz",
+        ["name"] = _("スタートアップ"),
+        ["description"] = _("A-10Cのランプからのスタートアップを練習します"),
+    },
+	{
+        ["file"] = "A-10C-Training-Takeoff and Handling.miz",
+        ["name"] = _("離陸と基本操作"),
+        ["description"] = _("離陸といくつかの基本的操縦を中高度にて練習します"),
+    },
+	{
+        ["file"] = "A-10C-Training-Navigation.miz",
+        ["name"] = _("ナビゲーション"),
+        ["description"] = _("ナビゲーションシステムの説明と操作の練習をします"),
+    },
+	{
+        ["file"] = "A-10C-Training-Landing.miz",
+        ["name"] = _("着陸"),
+        ["description"] = _("ILSアプローチによる着陸の練習と、シャットダウンの手順について説明します"),
+    },
+	{
+        ["file"] = "A-10C-Training-DSMS.miz",
+        ["name"] = _("デジタル・ストアズ・マネジメント・システム(DSMS)"),
+        ["description"] = _("デジタル・ストアズ・マネジメント・システムについて解説します"),
+    },
+	{
+        ["file"] = "A-10C-Training-TGP.miz",
+        ["name"] = _("AN/AAQ-28 LITENING-IIターゲティングポッド"),
+        ["description"] = _("AN/AAQ-28 LITENING-II 照準ポッドの練習を行います"),
+    },
+	{
+        ["file"] = "A-10C-Training-Guns and Rockets.miz",
+        ["name"] = _("GAU-8/Aと無誘導ロケット"),
+        ["description"] = _("GAU-8/Aガンと無誘導ロケットの扱いについて練習します"),
+    },
+    {
+        ["file"] = "A-10C-Training-Unguided Bombs.miz",
+        ["name"] = _("無誘導爆弾"),
+        ["description"] = _("無誘導爆弾の使い方について説明します"),
+    },
+	{
+        ["file"] = "A-10C-Training-PGM.miz",
+        ["name"] = _("精密誘導爆弾"),
+        ["description"] = _("精密誘導爆撃について説明します"),
+    },
+	{
+        ["file"] = "A-10C-Training-Maverick.miz",
+        ["name"] = _("AGM-65 Maverick"),
+        ["description"] = _("AGM-65 Maverickによる地上攻撃を練習します"),
+    },
+	{
+        ["file"] = "A-10C-Training-Air to Air.miz",
+        ["name"] = _("空対空"),
+        ["description"] = _("AN/AAQ-28 LITENING-II を使用してのAIM-9M 短距離IRミサイルと GAU-8/A ガンの使用を練習します"),
+    },
+    {
+        ["file"] = "A-10C-Training-Countermeasures.miz",
+        ["name"] = _("カウンターメジャー　「Countermeasures (CMS)」"),
+        ["description"] = _("敵からの攻撃に対する防御（対抗手段）について説明します"),
+    },
+} -- end of lessons
