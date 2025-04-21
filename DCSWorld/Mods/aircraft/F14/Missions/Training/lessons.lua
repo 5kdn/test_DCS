05c262229e6a8e3754c5902337ddc2123171d2af
5kdn <sakuden542+github@gmail.com>
2025-04-21 23:16:35 +0900

    - https://uu.getuploader.com/DCSJP1/download/176

---
+++ DCSWorld/Mods/aircraft/F14/Missions/Training/lessons.lua
@@ -0,0 +1,79 @@
+lessons =
+{
+	{
+        ["file"] = "F-14B tutorial cold start.miz",
+        ["name"] = _("基本 - パイロットコールドスタート"),
+        ["description"] = _("F-14 パイロット コールドスタートチュートリアル"),
+    },
+	{
+        ["file"] = "F-14B-RIO Cold Start.miz",
+        ["name"] = _("基本 -  RIOコールドスタート"),
+        ["description"] = _("F-14 RIO コールドスタートチュートリアル"),
+    },
+	{
+        ["file"] = "F-14B tutorial Shore-based Takeoff.miz",
+        ["name"] = _("基本 - 陸上からのテイクオフ"),
+        ["description"] = _("陸上でのテイクオフ手順を網羅したチュートリアル"),
+    },
+    {
+        ["file"] = "F-14B tutorial Shore-based Landing.miz",
+        ["name"] = _("基本 - 陸上へのランディング"),
+        ["description"] = _("陸上でのランディング手順を網羅したチュートリアル"),
+    },
+    {
+        ["file"] = "F-14B tutorial carrier-based Takeoff Case1.miz",
+        ["name"] = _("基本 - 空母からのケース1テイクオフ"),
+        ["description"] = _("空母でのケース1テイクオフ手順を網羅したチュートリアル"),
+    },
+    {
+        ["file"] = "F-14B tutorial carrier-based Case1.miz",
+        ["name"] = _("基本 - 空母へのケース1ランディング"),
+        ["description"] = _("空母でのケース1ランディング手順を網羅したチュートリアル"),
+    },
+    {
+        ["file"] = "F-14B tutorial Navigation.miz",
+        ["name"] = _("基本 - ナビゲーション"),
+        ["description"] = _("F-14の基本的なナビゲーションを網羅したチュートリアル"),
+    },   
+    {
+        ["file"] = "F-14B tutorial AA M61.miz",
+        ["name"] = _("空対空 - Guns Guns Guns"),
+        ["description"] = _("M61バルカンキャノンの空対空使用を網羅したチュートリアル"),
+    },
+    {
+        ["file"] = "F-14B tutorial AIM9.miz",
+        ["name"] = _("空対空 - フォックス-2　サイドワインダーミサイル"),
+        ["description"] = _("熱線追尾AIM-9サイドワインダーの使用を網羅したチュートリアル"),
+    },
+        {
+        ["file"] = "F-14B tutorial AG M61.miz",
+        ["name"] = _("空対地 - Guns"),
+        ["description"] = _("M61バルカンキャノンの空対地使用を網羅したチュートリアル"),
+    },
+    {
+        ["file"] = "F-14B tutorial AG CMP PLT.miz",
+        ["name"] = _("空対地 - コンピューターパイロット（CCIP）"),
+        ["description"] = _("コンピュータパイロット（CCIP）モードの使用を網羅したチュートリアル"),
+    },   
+    {
+        ["file"] = "F-14B-AWG-9-1.miz",
+        ["name"] = _("AN/AWG-9レーダー  - はじめに"),
+        ["description"] = _("AN/AWG-9レーダーを紹介し、関連するコントロールとパネルを詳述するチュートリアル"),
+    }, 
+    {
+        ["file"] = "F-14B-AWG-9-2.miz",
+        ["name"] = _("AN/AWG-9レーダー  - パルスモード"),
+        ["description"] = _("AN/AWG-9レーダーのパルスモードとその使用方法を詳述したチュートリアル"),
+    },
+    {
+        ["file"] = "F-14B-AWG-9-3.miz",
+        ["name"] = _("AN/AWG-9レーダー  - パルスドップラーモード"),
+        ["description"] = _("AN/AWG-9レーダーのパルスドップラーモードとその使用方法を詳述したチュートリアル"),
+    },
+    {
+        ["file"] = "F-14B-AWG-9-4.miz",
+        ["name"] = _("AN/AWG-9レーダー - MRL and IFF"),
+        ["description"] = _("MRLとIFFシステムを詳述したチュートリアル"),
+    }   
+} -- end of lessons
+
